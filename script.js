b711cbc6555759c8eea6425ac4a9673fcf47fc0a
IvanildoBorges <ivanildo.rap@gmail.com>
2024-02-05 17:53:49 -0300

    Adicionado lógica e html

---
+++ script.js
@@ -0,0 +1,33 @@
+function retornaSaldoDePartidasRankeadas(vitorias, derrotas) {
+    return vitorias - derrotas;
+}
+
+function mostraResultadoDasPartidas() {
+    let patente = '';
+    let vitorias = 85;
+    let derrotas = 5;
+    let saldo = retornaSaldoDePartidasRankeadas(vitorias, derrotas);
+    const titulo = document.querySelector('.container__content h1');
+
+    if (vitorias <= 10) {
+        patente = 'Ferro';
+    } else if (vitorias > 10 && vitorias < 21) {
+        patente = 'Bronze';
+    } else if (vitorias >= 21 && vitorias < 51) {
+        patente = 'Prata';
+    } else if (vitorias >= 51 && vitorias < 81) {
+        patente = 'Ouro';
+    } else if (vitorias >= 81 && vitorias < 91) {
+        patente = 'Diamante';
+    } else if (vitorias >= 91 && vitorias < 101) {
+        patente = 'Lendário';
+    } else if (vitorias >= 101) {
+        patente = 'Imortal';
+    }
+
+    titulo.innerHTML = `
+        O Herói tem de saldo de ${saldo} está no nível de ${patente}
+    `;
+}
+
+mostraResultadoDasPartidas();
